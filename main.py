b40a5b2483ff5b3dd0c45063f397ac684416c5a4
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2020-01-06 16:40:06 +0800

    remove direct in proxy mode

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ def main():
     print("Building customized load balance config...")
     load_balancer_policy = {"name": "LB-ALLPROXY", "type": "load-balance",
                             "url": usrconf["latency-test-url"],
-                            "interval": 300, "proxies": ["DIRECT"]
+                            "interval": 300, "proxies": []
                             }
     for proxies in service_provider_list:
         load_balancer_policy["proxies"].append(proxies)
